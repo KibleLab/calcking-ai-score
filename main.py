941b7897361c3de650e086ef1edc3e5096974e43
vulpes94 <rwr9857@gmail.com>
2024-09-26 16:56:05 +0900

    --

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 class OcrDto(BaseModel):
     image: str
 
-@app.post("/v1/ocr-number")
+@app.post("/v1/ocr")
 def ocr_number(ocrDto: OcrDto):
 
     folder = "imgs"
@@ -30,6 +30,51 @@ def ocr_number(ocrDto: OcrDto):
     save_base64_image(ocrDto.image, file)
 
     reader = easyocr.Reader(['ko', 'en'], gpu=True)
-    img = cv2.imread(file)
-    text = reader.readtext(img, detail=0, allowlist="0123456789+-x")
-    return {"result" : text}
+
+
+
+    # 자를 영역 지정 (x, y, width, height)
+    x, y, width, height = 0, 0, 250, 180  # 예시 값, 실제 이미지에 맞게 조정 필요
+
+    # 이미지 자르기
+    cropped_image = crop_image(file, x, y, width, height)
+
+
+
+    # img = cv2.imread(cropped_image)
+    textArr = reader.readtext(cropped_image, detail=0, allowlist="0123456789+-x")
+    
+    #배열을 문자열로 바꿈
+    text = "".join(textArr)
+
+    # 연산을 수행함.
+    anwser = calculate(text)
+
+    return {"result" : text + '=' + str(anwser)}
+
+
+def calculate(text):
+    operators = ['+', '-', 'x', '/']
+    for op in operators:
+        if op in text:
+            parts = text.split(op)
+            if len(parts) == 2:
+                a, b = int(parts[0]), int(parts[1])
+                if op == '+':
+                    return a + b
+                elif op == '-':
+                    return a - b
+                elif op == 'x':
+                    return a * b
+                elif op == '/':
+                    return a / b
+    return "유효한 연산이 아닙니다."
+
+def crop_image(image_path, x, y, width, height):
+    # 이미지 읽기
+    image = cv2.imread(image_path)
+    
+    # 지정된 영역 자르기
+    cropped_image = image[y:y+height, x:x+width]
+    
+    return cropped_image
